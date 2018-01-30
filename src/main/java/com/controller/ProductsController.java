8d7f54f31ebdb4ba4e7dd43a356fed50a87f582c
thaihiep098@gmail.com <thaihiep098@gmail.com>
2018-01-30 19:34:25 +0700

    delete product api

---
+++ src/main/java/com/controller/ProductsController.java
@@ -0,0 +1,58 @@
+package com.controller;
+
+import com.entity.ProductsEntity;
+import com.exception.ProductsNotFoundException;
+import com.service.ProductsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class ProductsController {
+
+    @Autowired
+    private ProductsService productsService;
+
+    @RequestMapping(value = "/product",method = RequestMethod.GET)
+    public ResponseEntity<List<ProductsEntity>> listAllProducts(){
+        List<ProductsEntity> productsEntities=productsService.findAll();
+        if(productsEntities.isEmpty()){
+            return new ResponseEntity<List<ProductsEntity>>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<ProductsEntity>>(productsEntities,HttpStatus.OK);
+    }
+    @RequestMapping(value ="/products/edit/{id}" ,method = RequestMethod.PUT)
+    public ResponseEntity<ProductsEntity> updateProduct(@PathVariable("id") int id,
+                                                        @RequestBody ProductsEntity productsEntity) throws ProductsNotFoundException {
+        System.out.print("Updating Product :" +id);
+        ProductsEntity currentProduct=productsService.getProductById(id);
+        if(currentProduct==null){
+            System.out.println("User with id :" + id + "not found");
+            return new ResponseEntity<ProductsEntity>(HttpStatus.NOT_FOUND);
+        }
+        currentProduct.setName(productsEntity.getName());
+        currentProduct.setDescription(productsEntity.getDescription());
+        currentProduct.setCreated(productsEntity.getCreated());
+        currentProduct.setPrice(productsEntity.getPrice());
+
+        productsService.updateProduct(currentProduct);
+        return new ResponseEntity<ProductsEntity>(currentProduct,HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/protected/v1.0/api/products/delete/{productId}", method = RequestMethod.DELETE)
+    public ResponseEntity<ProductsEntity> deleteProduct(@PathVariable final Integer productId ) throws ProductsNotFoundException {
+        ProductsEntity product = productsService.getProductById(productId);
+
+        if(product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        productsService.deleteProduct(product);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/service/ProductsService.java
@@ -0,0 +1,13 @@
+package com.service;
+
+import com.entity.ProductsEntity;
+import com.exception.ProductsNotFoundException;
+
+import java.util.List;
+
+public interface ProductsService {
+    List<ProductsEntity> findAll();
+    ProductsEntity getProductById(int id) throws ProductsNotFoundException;
+    public void updateProduct(ProductsEntity productsEntity);
+    public void deleteProduct(ProductsEntity productsEntity);
+}
